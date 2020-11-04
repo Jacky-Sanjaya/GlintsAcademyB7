4a5a5fa74bd14ae717d98b1aed5caf4d2d503140
Jacky-Sanjaya <kykho3@gmail.com>
2020-11-04 15:07:18 +0700

    code challenge @Jacky Sanjaya

---
+++ Week8/codeChallenge/App.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import HomeScreen from "./src/HomeScreen"
+import LoginScreen from "./src/LoginScreen"
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+
+}
